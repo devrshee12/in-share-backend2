4ba98e09f94ee3f757b80a6022fdd7ded65ac130
devarshee <ddevarshee12@gmail.com>
2022-06-24 17:58:19 +0530

    inital

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express") 
+const app = express();
+const PORT = process.env.PORT || 3000;
+const connectDB = require("./config/db");
+const path = require("path");
+const cors = require("cors");
+
+connectDB();
+
+//cors 
+
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+
+}
+
+app.use(cors(corsOptions));
+
+
+
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.set('views', path.join(__dirname, '/views'));
+app.set("view engine", "ejs");
+
+//routes
+app.use("/api/files", require("./routes/files"));       
+app.use("/files", require("./routes/show"))
+app.use("/files/download", require("./routes/download"));
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`listening on port number ${PORT}`);
+})
